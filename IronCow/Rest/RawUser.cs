c24f2d19d276dafdcb77765b97e69b6a1a3d8b5d
julianapena <julianapena@localhost>
2010-10-30 20:17:20 -0500

    A few bits of polishing: 1. Data is now deleted correctly when logging out 2. Log in browser is now bigger 3. Application icon is now the correct size 4. ListPage now disables adding tasks in smart lists

---
+++ IronCow/Rest/RawUser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Serialization;
+using System.Runtime.Serialization;
+
+namespace IronCow.Rest
+{
+    [DataContract]
+    public class RawUser
+    {
+        [XmlAttribute("id")]
+        [DataMember]
+        public int Id { get; set; }
+
+        [XmlAttribute("username")]
+        [DataMember]
+        public string UserName { get; set; }
+
+        [XmlAttribute("fullname")]
+        [DataMember]
+        public string FullName { get; set; }
+    }
+}
